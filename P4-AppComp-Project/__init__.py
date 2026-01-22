3eef1a15aac46da1d76b8694c49ad89e8274ff9f
Bunjy1 <2758304h@student.gla.ac.uk>
2026-01-22 18:54:54 +0000

    Update __init__.py

--- P4-AppComp-Project/__init__.py
+++ P4-AppComp-Project/__init__.py
@@ -1,1 +1,5 @@
-from P4-AppComp-Project import double
+
+from P4-AppComp-Project.P4-AppComp-Project.double-number import double
+from P4-AppComp-Project.P4-AppComp-Project.straight-line import straightline_fitter
+from P4-AppComp-Project.P4-AppComp-Project.polynomial import polynomial_fitter
+from P4-AppComp-Project.P4-AppComp-Project.periodic import periodic_fitter
