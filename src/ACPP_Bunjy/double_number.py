a7ca58b0c276b754c109213f033b549c45713c37
Bunjy1 <2758304h@student.gla.ac.uk>
2026-01-22 20:10:52 +0000

    Add imports for numpy, matplotlib, and scipy
    Added necessary imports for future enhancements.

--- src/ACPP_Bunjy/double_number.py
+++ src/ACPP_Bunjy/double_number.py
@@ -1,4 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+
 def double(input):
-  "Takes an input number and returns it times two."
+  ## Takes an input number and returns it times two.
   output = 2*input
   return(output)
