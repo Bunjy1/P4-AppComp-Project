415c9ac87faa6887cd0a1bccd2e2cf1a0f2e667b
Bunjy1 <2758304h@student.gla.ac.uk>
2026-01-29 18:12:11 +0000

    Add week3functions to module exports

--- src/ACPP_Bunjy/__init__.py
+++ src/ACPP_Bunjy/__init__.py
@@ -2,10 +2,28 @@ from .double_number import double
 from .straight_line import straightline_fitter
 from .polynomial import polynomial_fitter
 from .periodic import periodic_fitter
+from .week3functions import (
+    f1,
+    f2,
+    f3,
+    f_sum,
+    func,
+    signal_minimise,
+    svd_alg,
+    clustering
+)
 
 __all__ = [
     "double",
     "straightline_fitter",
     "polynomial_fitter",
     "periodic_fitter",
+    "f1",
+    "f2",
+    "f3",
+    "f_sum",
+    "func",
+    "signal_minimise",
+    "svd_alg",
+    "clustering",
 ]
