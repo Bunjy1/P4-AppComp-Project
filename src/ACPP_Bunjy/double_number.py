2189afce8acdeed11492191e5a0dc858f3910638
Bunjy1 <2758304h@student.gla.ac.uk>
2026-03-16 15:38:56 +0000

    Delete src/ACPP_Bunjy/double_number.py

--- src/ACPP_Bunjy/double_number.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy import optimize
-
-def double(input):
-  ## Takes an input number and returns it times two.
-  output = 2*input
-  return(output)
