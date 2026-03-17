a7ea6a5ec3ab995cfe7aa771e463d536014444d0
Bunjy1 <2758304h@student.gla.ac.uk>
2026-03-17 14:16:04 +0000

    Implement metrics for decomposition evaluation
    Added functions to calculate relative reconstruction error, explained variance ratio, and noise suppression metric for decomposition algorithms.

--- src/ACPP_Bunjy/decomp.py
+++ src/ACPP_Bunjy/decomp.py
@@ -13,13 +13,99 @@ from scipy import optimize
 from tqdm import tqdm
 import hyperspy.api as hs
 
-# For git upload eventually, include package definitions with function
+""" Functions """
+def rre(orig, clean):
+    """ Function to find the relative reconstruction error of a
+        decomposition algorithm. Typically used within the later
+        auto_decomp function, but can be applied manually.
 
+        Parameters
+        ----------
+        orig: numpy.nparray
+            Raw dataset before decomposition.
+        clean: numpy.nparray
+            Cleaned dataset after decomposition and reconstruction.
+
+        Returns
+        -------
+        rel_error: float
+            Relative reconstruction error of decomposition, see example notebook for details.
+        """
+    rel_error = np.linalg.norm(orig - clean) / np.linalg.norm(orig)
+    print('Relative reconstruction error: {}'.format(rel_error))
+    return(rel_error)
+
+def evr(orig, clean):
+    """ Function to find the explained variance ratio of a
+        decomposition algorithm. Typically used within the later
+        auto_decomp function, but can be applied manually.
+
+        Parameters
+        ----------
+        orig: numpy.nparray
+            Raw dataset before decomposition.
+        clean: numpy.nparray
+            Cleaned dataset after decomposition and reconstruction.
+
+        Returns
+        -------
+        evr: float
+            Explained variance ratio of decomposition, see example notebook for details.
+        """
+    evr_raw = cleaned_data.get_explained_variance_ratio().data
+    evr = evr_raw[:vectors].sum() # change value from 5 when implementing vector selection
+    print('Explained variance ratio: {}'.format(evr))
+    return(evr)
+
+def nsm(orig, clean):
+    """ Function to find the noise suppression metric of a
+        decomposition algorithm. Typically used within the later
+        auto_decomp function, but can be applied manually.
+
+        Parameters
+        ----------
+        orig: numpy.nparray
+            Raw dataset before decomposition.
+        clean: numpy.nparray
+            Cleaned dataset after decomposition and reconstruction.
+
+        Returns
+        -------
+        nsr: float
+            Noise suppression metric of decomposition, see example notebook for details.
+        """
+    orig_std = np.std(orig)
+    clean_std = np.std(clean)
+    nsr = clean_std / orig_std
+    print('Noise suppression metric: {}'.format(nsr))
+    return(nsr)
+    
 def auto_decomp(input_data, vectors, algorithm="SVD", metrics=False, plots=False):
 
-    # Function to automate decompostion functions, with the ability to choose number of reconstruction vectors and algorithm used.
-    # Can calculate metrics to evaluate goodness of cleanup, and plot the reconstruction and scree plot
-    # the input_data array must be a dataset already processed through hs.load (may change)
+    """ Function to automate and streamline Hyperspy decomposition
+        of input data. Data can be reconstructed with a desired number
+        of vectors, different UML algorithms, and otional returns of
+        the decomposition function plots and metrics relating to
+        goodness of fit.
+
+        Parameters
+        ----------
+        input_data: hyperspy.signals.Signal1D
+            Input raw data for decomposition, formatted in Hyperspy signal form.
+        vectors: int
+            Desired vectors to be used for signal reconstruction, for which all vectors up to the int value will be used.
+        algorithm: str, default='SVD'
+            The algorithm to be used by Hyperspy for decomposition, see Hyperspy documentation for details.
+        metrics: bool, optional
+            Argument to return metrics for evaluation of algorithm performance, including RRE, EVR, and NSM.
+        plots: bool, optional
+            Argument to plot decomposition and scree plot in the Hyperspy framework, again see HS documentation.
+
+        Returns
+        -------
+        Cleaned data: hyperspy.signals.Signal1D
+            Reconstructed data using set vectors, with noise removed.
+        """
 
     # Decomposition
     input_data.decomposition(algorithm=algorithm) # some algorithms need output_dimension specified?
@@ -30,20 +116,11 @@ def auto_decomp(input_data, vectors, algorithm="SVD", metrics=False, plots=False
         orig = input_data.data
         clean = cleaned_data.data
 
-        # Relative reconstruction error
-        rel_error = np.linalg.norm(orig - clean) / np.linalg.norm(orig)
-        print('Relative reconstruction error: {}'.format(rel_error))
+        
 
-        # Explained variance ratio
-        evr_raw = cleaned_data.get_explained_variance_ratio().data
-        evr = evr_raw[:vectors].sum() # change value from 5 when implementing vector selection
-        print('Explained variance ratio: {}'.format(evr))
+        
 
-        # Noise suppresion metric
-        orig_std = np.std(orig)
-        clean_std = np.std(clean)
-        nsr = clean_std / orig_std
-        print('Noise suppression metric: {}'.format(nsr))
+        
     
     # Finding recommended number of decomp vectors?
     # Base on an analysis of metrics and scree plot?
